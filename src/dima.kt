cefdfb54c1efacab0c919d4dcb326af32580b59e
N2A3P1O0 <cat.nadia23.10@gmail.com>
2020-02-17 20:03:49 +0700

    Initial commit

---
+++ src/dima.kt
@@ -0,0 +1,33 @@
+class Kvantorium{
+    var kvantums : Array<Kvantum> = arrayOf(Kvantum("It"),Kvantum("Geo"),Kvantum("Bio"),Kvantum( "Aero"),Kvantum( "Robo"),Kvantum( "VrAr"),Kvantum("Hitec"))
+}
+class Kvantum(s : String){
+    var it : Array<String> = arrayOf( "it1",)
+}
+class Groop(){
+
+}
+class Chel{
+
+}
+
+fun main(){
+
+   // var f: Array<String> = arrayOf("Pupkov", "Tikhomirov", "Cidefr")
+   // var n: Array<String> = arrayOf("Dmitriy", "Pasha", "Georgy")
+   // var s: Array<String> = arrayOf("Igorevich", "Georgiev", "Pupkovich")
+
+   // fun random(){
+   //     for (i in 1..100){
+    //        println(f [ (0..2).random() ] + "  " + n[(0..2).random()] + "  " + s[(0..2).random()])
+
+    //    }
+   // }
+
+   // println(random())
+
+}
+
+
+
+
